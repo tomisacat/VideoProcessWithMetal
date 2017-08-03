18a657c45698d82902f9d3eaf42a42e491efa592
tomisacat <wajmjjj@gmail.com>
2017-08-03 13:15:23 +0800

    Initial Commit

---
+++ VideoProcessWithMetal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoProcessWithMetal
+//
+//  Created by tomisacat on 03/08/2017.
+//  Copyright © 2017 tomisacat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
